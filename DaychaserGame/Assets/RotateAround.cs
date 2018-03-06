c3ae6f2e9d06016df3717d8c21927da4bb70e29a
Frederik Pedersen <frecodev@gmail.com>
2018-03-06 14:27:35 +0100

    some stupid rotation physics

---
+++ DaychaserGame/Assets/RotateAround.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+    public GameObject cube;
+    public Transform center;
+    public Vector3 axis = Vector3.up;
+    public Vector3 desiredPosition;
+    public float radius = 2.0f;
+    public float radiusSpeed = 0.5f;
+    public float rotationSpeed = 80.0f;
+
+    void Start()
+    {
+        center = cube.transform;
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+        radius = 2.0f;
+    }
+
+    void Update()
+    {
+        transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
+        desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+    }
+}

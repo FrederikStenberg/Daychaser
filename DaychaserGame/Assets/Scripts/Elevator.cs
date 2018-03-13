8bde14bfaf7bf5cebc5d1bde3876ffb3f5f9f8b7
Guflix - Thomas Boelt <thomas@boelthansen.dk>
2018-03-13 14:30:15 +0100

    ´DANK ELEVATORS I KOPI

--- DaychaserGame/Assets/Scripts/Elevator.cs
+++ DaychaserGame/Assets/Scripts/Elevator.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Elevator : MonoBehaviour {
 
-    public Transform movingtPlatform;
+    public Transform movingPlatform;
     public Transform position1;
     public Transform position2;
     public Vector3 newPosition;
-    private string currentState;
+    public string currentState;
     public float smooth;
     public float resetTime;
 
@@ -19,26 +18,21 @@ public class Elevator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        movingtPlatform.position = Vector3.Lerp(movingtPlatform.position, newPosition, smooth * Time.deltaTime);
+        movingPlatform.position = Vector3.Lerp(movingPlatform.position, newPosition, smooth * Time.deltaTime);
 	}
     void ChangeTarget()
     {
-        if (currentState == "Moving To Position 1")
+        if (currentState == "To1" || currentState == "")
         {
-            currentState = "Moving To Position 2";
+            currentState = "To2";
             newPosition = position2.position;
         }
-
-        else if (currentState == "Moving To Position 2")
+        else if (currentState == "To2")
         {
-            currentState = "Moving To Position 1";
+            currentState = "To1";
             newPosition = position1.position;
         }
-        else if (currentState == "")
-        {
-            currentState = "Moving To Position 2";
-            newPosition = position2.position;
-        }
+      
         Invoke("ChangeTarget", resetTime);
     }
 

0735239e24fa1f1252934e443c5660a18fc892f8
Guflix - Thomas Boelt <thomas@boelthansen.dk>
2018-03-13 14:00:16 +0100

    Some movement + ligh i Kopi

---
+++ DaychaserGame/Assets/Scripts/Elevator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+    public Transform movingtPlatform;
+    public Transform position1;
+    public Transform position2;
+    public Vector3 newPosition;
+    private string currentState;
+    public float smooth;
+    public float resetTime;
+
+	// Use this for initialization
+	void Start () {
+        ChangeTarget();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        movingtPlatform.position = Vector3.Lerp(movingtPlatform.position, newPosition, smooth * Time.deltaTime);
+	}
+    void ChangeTarget()
+    {
+        if (currentState == "Moving To Position 1")
+        {
+            currentState = "Moving To Position 2";
+            newPosition = position2.position;
+        }
+
+        else if (currentState == "Moving To Position 2")
+        {
+            currentState = "Moving To Position 1";
+            newPosition = position1.position;
+        }
+        else if (currentState == "")
+        {
+            currentState = "Moving To Position 2";
+            newPosition = position2.position;
+        }
+        Invoke("ChangeTarget", resetTime);
+    }
+
+}
---
+++ DaychaserGame/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour
+{
+    [HideInInspector]
+    public bool pressed = false;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Plate")
+        {
+            Debug.Log("LUL");
+            pressed = true;
+            //DO SOMETHING!
+
+        }
+        if (pressed == true)
+        {
+
+        }
+    }
+    void OnCollisionExit(Collision col)
+    {
+        pressed = false;
+        Debug.Log("OFF");
+    }
+}

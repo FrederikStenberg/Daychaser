97362744b8eafb3032ccea6deafa737264be9768
AlexanderSRasmussen <32093781+AlexanderSRasmussen@users.noreply.github.com>
2018-04-18 09:49:46 +0200

    Audio Manager

---
+++ DaychaserGame/Assets/Audio/AudioManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Dubstep");
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
